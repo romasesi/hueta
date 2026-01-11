8ab84b67770de496e403e999e693e6b17e4a2a96
romasesi <77851104+romasesi@users.noreply.github.com>
2026-01-11 13:52:20 +0300

    Add files via upload

---
+++ online.js
@@ -0,0 +1,60 @@
+(function () {
+  'use strict';
+
+  var plugin = 'Free Online Cinema';
+
+  var sources = [
+    {
+      title: 'Источник A',
+      getIframe: function (movie) {
+        // пример логики
+        return 'https://example.com/embed?title=' +
+          encodeURIComponent(movie.title) +
+          '&year=' + movie.year;
+      }
+    },
+    {
+      title: 'Источник B',
+      getIframe: function (movie) {
+        return 'https://example2.com/player/' + movie.imdb_id;
+      }
+    }
+  ];
+
+  Lampa.Component.add(plugin, {
+    name: plugin,
+    type: 'video',
+
+    onCreate: function () {
+      this.movie = this.activity.movie;
+      this.renderSources();
+    },
+
+    renderSources: function () {
+      var items = sources.map(function (s, i) {
+        return {
+          title: s.title,
+          index: i,
+          template: 'button'
+        };
+      });
+
+      this.activity.render({
+        title: 'Выберите источник',
+        items: items
+      });
+
+      this.activity.select = this.selectSource.bind(this);
+    },
+
+    selectSource: function (item) {
+      var source = sources[item.index];
+      var iframe = source.getIframe(this.movie);
+
+      Lampa.Player.play({
+        url: iframe,
+        type: 'iframe'
+      });
+    }
+  });
+})();
