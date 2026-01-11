296a5e184b450a7b1b88d0e10be7b98c02837c26
romasesi <77851104+romasesi@users.noreply.github.com>
2026-01-11 14:29:02 +0300

    Update online.js

--- online.js
+++ online.js
@@ -1,66 +1,40 @@
 (function () {
   'use strict';
 
-  var plugin = 'Free Online Cinema';
+  var plugin = 'Free Embed 2026';
 
   var sources = [
-    {
-      title: 'MultiEmbed (самый стабильный сейчас)',
-      getIframe: function (movie) {
-        var imdb = movie.imdb_id || movie.imdb || '';
-        if (!imdb) return 'https://via.placeholder.com/1280x720?text=Нет+IMDB';
-
-        var url = 'https://multiembed.mov/?video_id=' + imdb;
-        
-        // если сериал и есть сезон/серия (Lampa обычно передаёт в activity)
-        if (movie.season && movie.episode) {
-          url += '&s=' + movie.season + '&e=' + movie.episode;
-        }
-        
-        return url;
-      }
-    },
-    
-    // запасной — autoembed (часто живее, когда multiembed тормозит)
-    {
-      title: 'AutoEmbed',
-      getIframe: function (movie) {
-        var imdb = movie.imdb_id || movie.imdb || '';
-        if (!imdb) return null;
-        return 'https://autoembed.co/embed/movie/' + imdb.replace('tt', '');
-      }
-    }
+    {title: 'MultiEmbed', getUrl: m => m.imdb_id ? 'https://multiembed.mov/?video_id=' + m.imdb_id : null},
+    {title: 'SuperEmbed', getUrl: m => m.imdb_id ? 'https://superembed.stream/embed/movie/' + m.imdb_id.replace('tt','') : null},
+    {title: 'VidSrc.to',  getUrl: m => m.tmdb_id ? 'https://vidsrc.to/embed/movie/' + m.tmdb_id : null},
+    {title: 'AutoEmbed',  getUrl: m => m.imdb_id ? 'https://autoembed.co/embed/movie/' + m.imdb_id.replace('tt','') : null}
   ];
 
   Lampa.Component.add(plugin, {
     name: plugin,
     type: 'video',
-
     onCreate: function () {
       this.movie = this.activity.movie || {};
-      this.activity.render({
-        title: 'Выберите источник',
-        items: sources.map((s, i) => ({
-          title: s.title,
-          index: i,
-          template: 'button'
-        }))
-      });
+      var items = sources.map((s,i)=>({title: s.title, index: i, template: 'button'}))
+                        .filter(s=> s.title); // убираем пустые
+
+      if (items.length === 0) {
+        Lampa.Noty.show('Нет доступных ID для источников');
+        return;
+      }
+
+      this.activity.render({title: 'Выберите плеер', items: items});
 
       this.activity.select = function(item) {
-        var source = sources[item.index];
-        var url = source.getIframe(this.movie);
+        var src = sources[item.index];
+        var url = src.getUrl(this.movie);
 
         if (!url) {
-          Lampa.Noty.show('Нет нужного ID для этого источника');
+          Lampa.Noty.show('Источник не поддерживает этот фильм');
           return;
         }
 
-        Lampa.Player.play({
-          url: url,
-          type: 'iframe',
-          title: this.movie.title
-        });
+        Lampa.Player.launch({url: url, type: 'iframe', title: this.movie.title || 'Без названия'});
       }.bind(this);
     }
   });
